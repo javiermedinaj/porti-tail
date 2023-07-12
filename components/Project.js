d45907c331621e38bd39ed17a6b4508389e2daf5
Javier Medina <javier_j_medina@hotmail.com>
2023-07-11 21:52:37 -0300

    cv

--- components/Project.js
+++ components/Project.js
@@ -80,7 +80,7 @@ const Proyect = () => {
                         <Link href="/..">Volver atras</Link>
                     </button>
                     <button className="px-3 ml-3 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-blue-800 mt-2">
-                        <Link target='_blank' rel="noopener noreferrer" href="/curri2023abril.pdf">Ver curriculum</Link>
+                        <Link target='_blank' rel="noopener noreferrer" href="/cvhabilidades.pdf">Ver curriculum</Link>
                     </button>
                 </div>
             </div>
